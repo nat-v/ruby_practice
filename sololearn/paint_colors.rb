caca0762ffbdf27b08c18194895f4d270cddccd2
Nataliia Volynets <natasha99zt@gmail.com>
2022-01-11 20:57:36 +0200

    add paint_colors.rb

---
+++ sololearn/paint_colors.rb
@@ -0,0 +1,27 @@
+CANVAS_AND_BRUSHES = 40.0
+TAXES = 0.1
+PAINT = 5.0
+
+loop do 
+  puts "How many colors will you need to create a new masterpiece?"
+  begin
+    colors_amount = Integer(gets.chomp)
+  rescue ArgumentError
+    puts "Oops, seems like you entered a wrong number, I need an integer number. Please, try again:"
+    retry  
+  end 
+
+  colors_cost = colors_amount * PAINT
+  taxes_cost = (CANVAS_AND_BRUSHES + colors_cost) * TAXES
+  total_cost = taxes_cost + CANVAS_AND_BRUSHES + colors_cost
+
+  puts "Alright, let's see... The canvas and brushes will cost #{CANVAS_AND_BRUSHES}, #{colors_amount} colors of paint will be 5.0 each, so for all colors you need to pay #{colors_cost}."
+  puts "Almost forgot about the taxes! They will be #{taxes_cost}."
+  puts "Total is #{total_cost}"
+
+  puts "Do you want to try another amount of colors?(Y/N)"
+  try_again = gets.chomp.downcase
+  if try_again != "y"
+    break
+  end
+end

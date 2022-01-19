8946c64b0b40b84648871595b64da51cbe574df2
Nataliia Volynets <natasha99zt@gmail.com>
2022-01-19 20:17:04 +0200

    update auth_module.rb and student.rb files
    - Use Auth module in Student class

--- udemy/student.rb
+++ udemy/student.rb
@@ -1,4 +1,7 @@
+require './module/auth_module'
+
 class Student
+  include Auth
   attr_accessor :first_name, :last_name, :email, :username, :password
  
   def initialize(firstname, lastname, username, email, password)
@@ -11,9 +14,11 @@ class Student
  
   def to_s
     "First name: #{@first_name}, Last name: #{@last_name}, Username: #{@username}, email address: #{@email}"
-  end
- 
+  end 
 end
  
 student = Student.new("Natasha", "Volynets", "nat_v", "natasha@example.com", "123456789")
 puts student
+
+hashed_password = student.create_hash_digest(student.password) 
+puts "Hashed password: #{hashed_password}"

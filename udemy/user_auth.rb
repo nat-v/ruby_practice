6afeb9c474ed00d0a3afd53a44b30fb86b48c704
Nataliia Volynets <natasha99zt@gmail.com>
2022-01-19 16:38:13 +0200

    update user_auth.rb
    Create authenticate_user method to check user credentials

--- udemy/user_auth.rb
+++ udemy/user_auth.rb
@@ -30,4 +30,18 @@ def create_secure_users(list_of_users)
   list_of_users
 end
 
-puts create_secure_users(users)
+def authenticate_user(username, password, list_of_users)
+  list_of_users.each do |user_record|
+    if user_record[:username] == username &&
+       verify_hash_digest(user_record[:password]) == password
+      return "Authentication successful #{user_record}"
+    else 
+      return "Credentials were not correct"
+    end
+  end
+end
+
+new_users = create_secure_users(users)
+puts new_users
+
+puts authenticate_user("natasha", "password1", new_users)

b849a01deb9470ed8ca66bb97cfb444c819900b3
Nataliia Volynets <natasha99zt@gmail.com>
2022-01-19 16:13:02 +0200

    create udemy folder and add student.rb
    Learning OOP:
    - classes;
    - attributes;
    - getters & setters.

---
+++ udemy/student.rb
@@ -0,0 +1,19 @@
+class Student
+  attr_accessor :first_name, :last_name, :email, :username, :password
+ 
+  def initialize(firstname, lastname, username, email, password)
+    @first_name = firstname
+    @last_name = lastname
+    @username = username
+    @email = email
+    @password = password
+  end
+ 
+  def to_s
+    "First name: #{@first_name}, Last name: #{@last_name}, Username: #{@username}, email address: #{@email}"
+  end
+ 
+end
+ 
+student = Student.new("Natasha", "Volynets", "nat_v", "natasha@example.com", "123456789")
+puts student
